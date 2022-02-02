64cbc513c791e1c01c4e0112805f6b557845e178
Vego <cx66001@gmail.com>
2022-02-02 11:21:11 +0300

    Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main (String [] args)    {
+        System.out.println("Alex");
+        System.out.println("02.02.2022");
+    }
+}
